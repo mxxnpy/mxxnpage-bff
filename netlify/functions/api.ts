a94976a0bde218cfc84c19b5df97530c479fd1a2
Ayu <ayu.leandro@icloud.com>
2025-03-18 11:51:08 -0300

    paia de mais

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,54 +1,16 @@
-import { Handler, HandlerEvent, HandlerContext, HandlerResponse } from '@netlify/functions';
+import { Handler } from '@netlify/functions';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../../src/app.module';
-import { ValidationPipe } from '@nestjs/common';
-import * as cookieParser from 'cookie-parser';
-import { SwaggerBuilder } from '../../src/config/swagger.config';
 import serverless from 'serverless-http';
 
-let cachedHandler: any;
+let cachedHandler: Handler;
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  
-  // Enable CORS for GitHub Pages and local development
-  app.enableCors({
-    origin: [
-      'https://mxxnpy.github.io',
-      'https://mxxnpy.github.io/mxxnpage/#/home',
-      'https://mxxnpy.github.io/mxxnpage/#/home/',
-      'https://mxxnpy.github.io/mxxnpage/#/spotify',
-      'https://mxxnpy.github.io/mxxnpage/#/project',
-      'https://mxxnpy.github.io/mxxnpage/#/',
-      'https://mxxnpy.github.io/mxxnpage',
-      'https://mxxnpy.github.io/mxxnpage/', 
-      'https://mxxnpy.github.io/mxxnpage/browser',
-      'https://mxxnpy.github.io/mxxnpage/browser/'
-    ],
-    credentials: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
-
-  app.use(cookieParser());
-  app.setGlobalPrefix('backend');
-  app.useGlobalPipes(new ValidationPipe({
-    transform: true,
-    whitelist: true,
-    forbidNonWhitelisted: true,
-  }));
-
-  return serverless(app.getHttpAdapter().getInstance());
-}
-
-export const handler: Handler = async (event: HandlerEvent, context: HandlerContext): Promise<HandlerResponse> => {
+export const handler: Handler = async (event, context) => {
   if (!cachedHandler) {
-    cachedHandler = await bootstrap();
+    const app = await NestFactory.create(AppModule);
+    app.setGlobalPrefix('backend');
+    await app.init();
+    cachedHandler = serverless(app.getHttpAdapter().getInstance());
   }
-  
-  // Call the serverless handler and ensure it returns a proper HandlerResponse
-  const response = await cachedHandler(event, context);
-  return response as HandlerResponse;
+  return cachedHandler(event, context);
 };
