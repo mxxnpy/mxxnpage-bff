6b31325c00487ac99bf1d41ca4591965dedc8c8b
Ayu <ayu.leandro@icloud.com>
2025-03-18 20:36:02 -0300

    scalar-api display fix

--- netlify/functions/esbuild_config.js
+++ netlify/functions/esbuild_config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  external: ['class-transformer/storage'],
+  external: ['class-transformer/storage', '@scalar/nestjs-api-reference', '@scalar/types', '@scalar/openapi-types'],
   define: {
     'process.env.NODE_ENV': '"production"'
   }
